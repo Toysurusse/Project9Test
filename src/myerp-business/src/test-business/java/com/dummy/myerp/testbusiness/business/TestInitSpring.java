a92f3427152606c9fdde829ed20c01653388bc99
leb yep <max.leboiteux@gmail.com>
2018-10-19 11:59:49 +0200

    MAJ TestInit

--- src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/TestInitSpring.java
+++ src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/TestInitSpring.java
@@ -24,8 +24,6 @@ public class TestInitSpring extends BusinessTestCase {
      */
     @Test
     public void testInit() {
-        SpringRegistry.init();
-        assertNull(SpringRegistry.getBusinessProxy());
-        assertNull(SpringRegistry.getTransactionManager());
+
     }
 }
