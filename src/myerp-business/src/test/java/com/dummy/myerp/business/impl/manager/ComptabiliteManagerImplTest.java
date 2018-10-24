cb89f1cb254757e180432e92eb528720bf4870fd
leb yep <max.leboiteux@gmail.com>
2018-10-24 14:40:38 +0200

    Test Okay travis ?

--- src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -37,7 +37,6 @@ public class ComptabiliteManagerImplTest extends BusinessTestCase {
         SpringRegistry.getTransactionManager().beginTransactionMyERP();
         BusinessProxy business = BusinessProxyImpl.getInstance(SpringRegistry.getDaoProxy(),SpringRegistry.getTransactionManager());
         ComptabiliteManagerImpl.configure( SpringRegistry.getBusinessProxy(), SpringRegistry.getDaoProxy(), SpringRegistry.getTransactionManager());
-        SpringRegistry.getBusinessProxy().getComptabiliteManager();
     }
 
 
